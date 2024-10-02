de5b30b052f235e057e75e3123891242f4816072
Bo.Cui <Bo.Cui@noaa.gov>
2024-10-02 17:25:31 -0400

    i20241002 update bufr driver

--- driver/product/check_nc.sh
+++ driver/product/check_nc.sh
@@ -1,11 +1,18 @@
 
 PDY=20200213
 
-COM_d=/lfs/h2/emc/vpppg/noscrub/bo.cui/BUFR/data_bufr_ctl/hr3scoutC768
-COM_d=/lfs/h2/emc/vpppg/noscrub/bo.cui/BUFR/data_bufr_ctl/hr3scoutC1152
+COM_d1=/lfs/h2/emc/vpppg/noscrub/bo.cui/BUFR/data_bufr_ctl/hr3scoutC768
+COM_d2=/lfs/h2/emc/vpppg/noscrub/bo.cui/BUFR/data_bufr_ctl/hr3scoutC1152
 
 
 dir=gfs.${PDY}/00/model_data/atmos/history
 file=gfs.t00z.atmf024.nc
+
+ls $COM_d1/$dir
+
+echo 
+ls $COM_d2/$dir
+
+exit
 ncdump -h $COM_d/$dir/$file
 
